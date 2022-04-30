6c195a862275e28542bd58c25c9703efa3217120
Arun Jangir <jangirarun786@gmail.com>
2022-04-30 07:51:49 +0300

    updated launch script

--- 00-Dry-Run/run.sh
+++ 00-Dry-Run/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-jobName="dry-run"
+
+# run with [--dry or -d] for a quick dry run
+echo -e "#> Usage\n    run with [--dry or -d] for a quick dry run\n#<"
+
 wd=${PWD}
-calcDIR=${wd}/calc-$jobName
+calcDIR=${wd}/calc
 dataDIR=${wd}/data
 
 # make any required dirs
@@ -12,7 +15,13 @@ cd $dataDIR
 cp INCAR OPTCELL POSCAR POTCAR KPOINTS run.sbatch $calcDIR/
 
 cd $calcDIR
-chmod +x ./run.sbatch
-./run.sbatch
+sed -i "s/__jobName/dry-run/" "$calcDIR/run.sbatch"
 
 # Stop it after 10 to 15 seconds. 
+# run run.sbatch with [--dry or -d] for a quick dry run
+if [[ $1 == "--dry" || $1 == "-d" ]]; then
+    bash run.sbatch $1
+else
+    source /ibex/scratch/jangira/root/sbatchh.sh
+    sbatchh run.sbatch
+fi
--- 03-Bader-Charge/example/run.sh
+++ 03-Bader-Charge/example/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# run with [--dry or -d] for a quick dry run
+echo -e "#> Usage\n    run with [--dry or -d] for a quick dry run\n#<"
 
 wd=${PWD}
 calcDIR=${wd}/calc
@@ -9,9 +11,13 @@ mkdir -p ${calcDIR}
 
 # Copy required files
 cd $dataDIR
-
 cp INCAR OPTCELL POSCAR POTCAR KPOINTS chgsum.pl bader run.sbatch $calcDIR/
 
 cd $calcDIR
-# bash run.sbatch
-sbatch run.sbatch
+# run run.sbatch with [--dry or -d] for a quick dry run
+if [[ $1 == "--dry" || $1 == "-d" ]]; then
+    bash run.sbatch 
+else
+    source /ibex/scratch/jangira/root/sbatchh.sh
+    sbatchh run.sbatch
+fi
